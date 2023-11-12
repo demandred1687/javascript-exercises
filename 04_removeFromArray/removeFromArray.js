fa6e65e390526b57f8c4d6dce4d8621f624a7530
demandred1687 <149145786+demandred1687@users.noreply.github.com>
2023-11-12 18:33:02 +0800

    exercise number 4 started

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...x) {
+let newArr = [...arr];
+return newArr.filter((val) => {return x.indexOf(val) == -1;})
 };
-
+console.log([1,2,3],3);
 // Do not edit below this line
 module.exports = removeFromArray;
