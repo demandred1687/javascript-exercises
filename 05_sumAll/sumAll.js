feb610edf0bb4907e80f7c1db148187dc3ee73e3
demandred1687 <149145786+demandred1687@users.noreply.github.com>
2023-11-17 00:40:13 +0800

    exercise number 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+if (typeof x !== 'number' | typeof y !== 'number' | x*y < 0) {
+    return 'ERROR';
+}
+let arr = [];
+    if (x < y) {
+        for (let i = x; i <= y; i++) {
+            arr.push(i);
+        }
+    } else {
+        for (let i = y; i <= x; i++) {
+            arr.push(i);
+        }
+    }
+return arr.reduce((a, v) => {a+=v; return a;});
 };
 
 // Do not edit below this line
